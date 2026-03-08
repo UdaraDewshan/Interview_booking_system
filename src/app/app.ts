dc318d82bc1e797676f482ebecb2953491d69cf5
UdaraDewshan <dewudara@gmail.com>
2026-03-09 01:37:06 +0530

    Change login page interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavBar } from "./components/nav-bar/nav-bar";
 import { Dashboard } from "./pages/dashboard/dashboard";
 
@@ -12,4 +12,15 @@ import { Dashboard } from "./pages/dashboard/dashboard";
 })
 export class App {
   protected readonly title = signal('booking_system');
+  showNavBar = true;
+
+  constructor(private router: Router){
+    this.router.events.subscribe((event) => {
+        if(event instanceof NavigationEnd){
+          this.showNavBar =!(event.url === '/login' || event.url  === '/');
+        }
+    });
+  }
+
+
 }
